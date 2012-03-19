a5e51e425bbc43797bda5b472a50f5c5fefd485a
Mark Maglana <mmaglana@gmail.com>
2012-03-20 05:40:16 +0800

    Add step definitions

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,3 @@
+Given /^I am logged in as a sysadmin of that project$/ do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/step_definitions/project_steps.rb
@@ -0,0 +1,8 @@
+Given /^An? '(.+)' project exists$/ do |project_name|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^The following developers exist in the project:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
