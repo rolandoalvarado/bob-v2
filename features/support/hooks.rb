b1043798d3ac9e9defc54ebf26f48d6aff248719
Mark Maglana <mmaglana@gmail.com>
2012-03-23 17:48:38 +0800

    Take screenshot and embed to report on scenario failure

---
+++ features/support/hooks.rb
@@ -0,0 +1,21 @@
+require 'fileutils'
+
+def tmp_screenshots_dir
+  File.join(File.expand_path("../../../23452344317467_tmp_screenshots_dir", __FILE__))
+end
+
+AfterConfiguration do |config|
+  FileUtils.rm_rf(tmp_screenshots_dir) if Dir.exists?(tmp_screenshots_dir)
+  Dir.mkdir(tmp_screenshots_dir)
+end
+
+After do |scenario|
+  if scenario.failed? && page.driver.respond_to?('browser')
+    page.driver.browser.save_screenshot(File.join(tmp_screenshots_dir, "scenario.#{__id__}.png"))
+    embed(File.join(tmp_screenshots_dir, "scenario.#{__id__}.png"), "image/png", "Screenshot")
+  end
+end
+
+at_exit do
+  FileUtils.rm_rf(tmp_screenshots_dir)
+end
