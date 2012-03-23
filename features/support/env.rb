313d5aecc87a0187992725aca7250aed151bedad
Mark Maglana <mmaglana@gmail.com>
2012-03-23 08:50:22 +0800

    Add config file support

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,22 @@
 require 'rubygems'
 require 'bundler/setup'
 
+config_path = File.expand_path('../../../config.yml', __FILE__)
+
+if File.exists? config_path
+  config = YAML.load_file(File.open(config_path, 'r+'))
+else
+  puts "WARNING: I couldn't find #{config_path}"
+  config = {}
+end
+
+if config['host'].nil? || config['host'].empty?
+  print "Which mCloud environment would you like to verify? "
+  config['host'] = STDIN.gets.chomp
+end
+
+config['host'] = "http://#{config['host']}" if config['host'].match(/^http/).nil?
+
 # Overrides the method +method_name+ in +obj+ with the passed block
 def override_method(obj, method_name, &block)
   # Get the singleton class/eigenclass for 'obj'
