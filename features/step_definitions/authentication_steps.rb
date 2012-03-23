98ae9de1a5093fc573b329286d260447a72dc70d
Mark Maglana <mmaglana@gmail.com>
2012-03-23 22:29:10 +0800

    Authentication steps

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,9 +1,29 @@
+# This step definition is not very well designed.
+# To improve it, the test should have a secret access
+# to the backend that allows it to create the user
+# on the fly.
 Given /^The following user exists:$/ do |table|
-  credentials = table.hashes[0]
+  # credentials = table.hashes[0]
+  #
+  # visit('/')
+  # fill_in 'username', :with => credentials['Username']
+  # fill_in 'password', :with => credentials['Password']
+  # click_button 'submit'
+  #
+  # logged_in?.should be_true
+  #
+  # click_link 'Logout' # Ensure that we start with a new session in subsequent steps of the scenario
+end
 
+When /^s?he logs in with the following credentials: (.*), (.*)$/ do |username, password|
   visit('/')
-  fill_in 'username', :with => credentials['Username']
-  fill_in 'password', :with => credentials['Password']
+  fill_in 'username', :with => username
+  fill_in 'password', :with => password
   click_button 'submit'
-  logged_in?.should be_true
+end
+
+Then /^s?he will be redirected to (.+)$/ do |page|
+  page = "" if page == "login"
+
+  current_path.should == "/#{page}"
 end
