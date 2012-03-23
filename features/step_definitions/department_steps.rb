fda8161d97b302922bd9e53c9c4ff39210e952c6
Mark Maglana <mmaglana@gmail.com>
2012-03-23 22:28:30 +0800

    Rename department to project

--- features/step_definitions/department_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^An? '(.+)' department exists in the system$/ do |department_name|
-  pending # express the regexp above with the code you wish you had
-end
-
-Given /^The following users exist in the department:$/ do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
-end
