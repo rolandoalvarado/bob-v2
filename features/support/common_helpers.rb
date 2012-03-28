7ff1b19dbf17679bf8abfe2b5e5a18c3b1e74107
Mark Maglana <mmaglana@gmail.com>
2012-03-28 11:40:49 +0800

    Add get_config_file

--- features/support/common_helpers.rb
+++ features/support/common_helpers.rb
@@ -39,6 +39,16 @@ def ensure_user_exists(info)
   logout
 end
 
+def get_config_file
+  return @config if @config
+  config_path = File.expand_path('../../../config.yml', __FILE__)
+
+  if File.exists? config_path
+    @config = YAML.load_file(File.open(config_path, 'r+'))
+  end
+  @config
+end
+
 def login_as_admin
   config = get_config_file
   error_message = ""
