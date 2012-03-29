ee4eabba62e3850ee6ae8ebec02ced4136fe12da
James Josef <jjosef@James-Josefs-MacBook.local>
2012-03-29 10:28:39 +0800

    Removed '#' in create_a_user.feature Removed whens in form_interaction.rb Created user_steps.rb

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,24 @@
+When /^(?:he|she) tries to create a user in her project$/ do
+  steps %{
+    visit Users page
+    fill in name with Jheff
+    fill in email with jvicedo@mail.com
+    fill in password ASDF
+    fill in password confirmation with ASDF
+    select first item in the projects drop down
+    click on save
+  }
+end
+
+When /^(?:he|she) tries to create a user with (.+), (.+), (.+), and (.+)$/ do |name, email, password, password_conf|
+  steps %{
+    visit User page
+    fill in name with #{name}
+    fill in email with #{email}
+    fill in password with #{password}
+    fill in password confirmation with #{password_conf}
+    select first item in the projects drop down
+    click on save
+  } 
+end
+
