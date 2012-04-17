636bc273f0cfb84181d19f2d234d5d3a3c270409
Mark Maglana <mmaglana@gmail.com>
2012-04-17 08:36:38 +0800

    Extract custom formatter and convert to a gem

---
+++ features/support/require_gems.rb
@@ -0,0 +1,25 @@
+# For gems that I am still heavily developing, it's easier for me to update
+# and test them if I symlink to them directly. However, for others who just
+# use the gems listed below, they're better off just requiring the gem
+# directly. So I automated the process of deciding how to require the rb files.
+# You normally don't have to edit this file AND you don't have to require
+# the gems anywhere else.
+#
+# --Mark (mmaglana@morphlabs.com)
+
+gems = [
+  { :name => 'activepage',      :symlink => 'activepage'                    },
+  { :name => 'diego_formatter', :symlink => 'cucumber/formatter/relaxdiego' }
+]
+
+gems.each do |gem_info|
+  symlink = File.expand_path("../#{ gem_info[:symlink] }", __FILE__)
+
+  if Dir.exists? symlink
+    Dir["#{ symlink }/*.rb"].each do |file|
+      require_relative file
+    end
+  else
+    require gem_info[:name]
+  end
+end
