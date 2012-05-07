281b732fcf9eff561503dd00db9e6055688edb5d
Mark Maglana <mmaglana@gmail.com>
2012-05-07 15:34:28 +0800

    Add spacing to stepdefs

--- features/step_definitions/projects_steps.rb
+++ features/step_definitions/projects_steps.rb
@@ -26,11 +26,15 @@ Given /^At least (\d+) images? should be available for use in the project$/ do |
   end
 end
 
+
+
 Given /^The project has (\d+) instances$/ do |number_of_instances|
   number_of_instances = number_of_instances.to_i
   compute_service     = ComputeService.session
   total_instances     = compute_service.ensure_project_instance_count(@project, number_of_instances)
 end
+
+
 
 Given /^I have a role of (.+) in the project$/ do |role_name|
   user_attrs       = CloudObjectBuilder.attributes_for(
@@ -71,6 +75,8 @@ Given /^I am authorized to create projects$/ do
   pending # express the regexp above with the code you wish you had
 end
 
+
+
 Given /^a user named Arya Stark exists in the system$/ do
   pending # express the regexp above with the code you wish you had
 end
@@ -84,13 +90,19 @@ When /^I create a project with attributes My Awesome Project, Another project$/ 
   pending # express the regexp above with the code you wish you had
 end
 
+
+
 When /^I create a project with attributes My Awesome Project, \(None\)$/ do
   pending # express the regexp above with the code you wish you had
 end
 
+
+
 When /^I create a project with attributes \(None\), Another project$/ do
   pending # express the regexp above with the code you wish you had
 end
+
+
 
 When /^I create a project$/ do
   pending # express the regexp above with the code you wish you had
@@ -105,6 +117,8 @@ Then /^I Cannot Create a project$/ do
   pending # express the regexp above with the code you wish you had
 end
 
+
+
 Then /^I Can Create a project$/ do
   project_name = Unique.name("DPBLOG-9-1")
   steps %{
@@ -116,17 +130,25 @@ Then /^I Can Create a project$/ do
   }
 end
 
+
+
 Then /^I can view that project$/ do
   pending # express the regexp above with the code you wish you had
 end
+
+
 
 Then /^Arya Stark cannot view that project$/ do
   pending # express the regexp above with the code you wish you had
 end
 
+
+
 Then /^the project will be Created$/ do
   pending # express the regexp above with the code you wish you had
 end
+
+
 
 Then /^the project will be Not Created$/ do
   pending # express the regexp above with the code you wish you had
