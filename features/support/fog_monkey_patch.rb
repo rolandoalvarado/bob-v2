ef32fe409ec5a66a601ade4340695343c4c890ff
Mark Maglana <mmaglana@gmail.com>
2012-05-07 15:32:10 +0800

    Use grant_user_role and revoke_user_role

--- features/support/fog_monkey_patch.rb
+++ features/support/fog_monkey_patch.rb
@@ -24,11 +24,11 @@ module Fog
       end # class Users
 
       class Tenant < Fog::Model
-        def add_user_role(user_id, role_id)
+        def grant_user_role(user_id, role_id)
           connection.add_user_to_tenant(self.id, user_id, role_id)
         end
 
-        def remove_user_role(user_id, role_id)
+        def revoke_user_role(user_id, role_id)
           connection.remove_user_from_tenant(self.id, user_id, role_id)
         end
       end
