3f211aa41de05112957a87d0fe0c15614fa8d12d
Mark Maglana <mmaglana@gmail.com>
2012-05-08 20:46:48 +0800

    Cleanup some stepdefs

--- features/step_definitions/projects_steps.rb
+++ features/step_definitions/projects_steps.rb
@@ -125,23 +125,14 @@ end
 # WHENs
 #=================
 
-When /^Fill in the (.+) field with$/ do |field|
-  #This step is called when there is no argument.
-  #So we ignore the step. 
-end
+When /^I create a project with attributes (.*), (.*)$/ do |name, desc|
+  attrs = CloudObjectBuilder.attributes_for(
+            :project,
+            :name        => name.downcase == '(none)' ? name : Unique.name(name),
+            :description => desc
+          )
 
-When /^I create a project with attributes (.*), (.*)$/ do |pname,pdesc|
-  if pname.downcase == "(none)"
-    pname = ""
-  else
-    pname = Unique.name(pname)
-    attributes = CloudObjectBuilder.attributes_for(:tenant, :name => pname)
-    IdentityService.session.delete_tenant(attributes)
-  end
-
-  if pdesc.downcase == "(none)"
-    pdesc = ""
-  end
+  IdentityService.session.delete_tenant(attrs)
 
   steps %{
     * Click the logout button if currently logged in
@@ -153,24 +144,42 @@ When /^I create a project with attributes (.*), (.*)$/ do |pname,pdesc|
 
     * Visit the projects page
 
-    * Click the create_project button
-    * Fill in the project_name field with #{pname}
-    * Fill in the project_description field with #{pdesc}
-    * Click the save_project button
+    * Click the create project button
+    * Fill in the project name field with #{ attrs.name }
+    * Fill in the project description field with #{ attrs.description }
+    * Click the save project button
   }
-  if pname == ""
-    @project_name = "(none)"
-  else
-    @project_name = pname
-  end
 
-
+  # Make the project name available to subsequent steps
+  @project_attrs = attrs
 end
 
 When /^I create a project$/ do
+  attrs = CloudObjectBuilder.attributes_for(
+            :project,
+            :name => Unique.name('project')
+          )
+
+  IdentityService.session.delete_tenant(attrs)
+
   steps %{
-    * I create a project with attributes DPBLOG91A, "This project is created by cucumber"
+    * Click the logout button if currently logged in
+
+    * Visit the login page
+    * Fill in the username field with #{ @current_user.name }
+    * Fill in the password field with #{ @current_user.password }
+    * Click the login button
+
+    * Visit the projects page
+
+    * Click the create project button
+    * Fill in the project name field with #{ attrs.name }
+    * Fill in the project description field with #{ attrs.description }
+    * Click the save project button
   }
+
+  # Make the project name available to subsequent steps
+  @project_attrs = attrs
 end
 
 
@@ -189,17 +198,18 @@ Then /^I Cannot Create a project$/ do
     * Click the login button
 
     * Visit the projects page
-
-    * the create project button is disabled
+    * The create project button should be disabled
   }
 end
 
 
-
 Then /^I Can Create a project$/ do
-  project_name = Unique.name("DPBLOG91B")
-  attributes = CloudObjectBuilder.attributes_for(:tenant, :name => project_name)
-  IdentityService.session.delete_tenant(attributes)
+  attrs = CloudObjectBuilder.attributes_for(
+            :project,
+            :name => Unique.name('project')
+          )
+
+  IdentityService.session.delete_tenant(attrs)
 
   steps %{
     * Click the logout button if currently logged in
@@ -211,57 +221,35 @@ Then /^I Can Create a project$/ do
 
     * Visit the projects page
 
-    * Click the create_project button
-    * Fill in the project_name field with #{project_name}
-    * Fill in the project_description field with "This project is created by cucumber."
-    * Click the save_project button
+    * Click the create project button
+    * Fill in the project name field with #{ attrs.name }
+    * Fill in the project description field with #{ attrs.description }
+    * Click the save project button
 
     * Visit the projects page
-    * A project named #{project_name} exists
+    * The #{ attrs.name } project should be visible
   }
 
+  @project_attrs = attrs
 end
 
-Then /^A project named (.+) exists$/ do |project_name|
-  if project_name.downcase == "(none)" 
-    project_name = ""
-  end
-  unless @current_page.has_project_link?( name: project_name )
-    raise ("project #{project_name} should exist, but it doesn't! user is #{@current_user.name} ")
-  end
-end
-
-Then /^A project named (.+) does not exist$/ do |project_name|
-  if project_name.downcase == "(none)" 
-    project_name = ""
-  end
-  if @current_page.has_project_link?( name: project_name )
-    raise ("project #{project_name} should not exist, but it does! Username is #{@current_user.name}")
-  end
-end
-
-Then /^the (.+) button is disabled$/ do |button_name|
-  if @current_page.has_content?(button_name)
-    raise ("#{button name} button should not exist. but it is.")
-  end
-end
 
 Then /^I can view that project$/ do
   steps %{
     * Visit the projects page
-    * A project named #{@project_name} exists
+    * The #{ @project_attrs.name } project should be visible
   }
 end
 
 
-
 Then /^Arya Stark cannot view that project$/ do
   user_attrs       = CloudObjectBuilder.attributes_for(
-                                                       :user,
-                                                       :name => Unique.username('aryastark')
-                                                       )
+                       :user,
+                       :name => Unique.username('aryastark')
+                     )
   identity_service = IdentityService.session
   user             = identity_service.ensure_user_exists(user_attrs)
+
   steps %{
     * Click the logout button if currently logged in
 
@@ -271,34 +259,23 @@ Then /^Arya Stark cannot view that project$/ do
     * Click the login button
 
     * Visit the projects page
-    * A project named #{@project_name} does not exist
+    * The #{ @project_attrs.name } project should not be visible
   }
-
 end
 
 
-
-Then /^the project will be Created$/ do
+Then /^the project will be [Cc]reated$/ do
   steps %{
     * Visit the projects page
-    * A project named #{@project_name} exists
+    * The #{ @project_attrs.name } project should be visible
   }
 end
 
 Then /^the project will be Not Created$/ do
-
   # current_page should still have a new project form
   # new project form should have the error message "This field is required".
-  if !@current_page.has_new_project_name_error_span? && !@current_page.has_new_project_description_error_span?
-    raise ("#{@project_name} should have new_project_name_error. but not.")    
+  if !@current_page.has_new_project_name_error_span? &&
+     !@current_page.has_new_project_description_error_span?
+    raise ("The project should not have been created, but it seems that it was.")
   end
-
-end
-
-Then /^I Can Delete the instance$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I Cannot Delete the instance$/ do
-  pending # express the regexp above with the code you wish you had
 end
