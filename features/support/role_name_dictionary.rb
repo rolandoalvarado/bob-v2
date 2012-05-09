8e495805915d166e7a88bc0b2f3342869bdfc27a
Mark Maglana <mmaglana@gmail.com>
2012-05-09 15:02:16 +0800

    Add error checking in role_name_dictionary

--- features/support/role_name_dictionary.rb
+++ features/support/role_name_dictionary.rb
@@ -2,22 +2,26 @@ module RoleNameDictionary
   DB_NAME       = :db_name
   FRIENDLY_NAME = :friendly_name
 
+  def self.roles
+    [
+      { DB_NAME => 'admin',  FRIENDLY_NAME => 'Project Manager' },
+      { DB_NAME => 'admin',  FRIENDLY_NAME => 'System Admin'    },
+      { DB_NAME => 'Member', FRIENDLY_NAME => 'Member'          }
+    ]
+  end
+
   def self.db_name(friendly_name)
     role = roles.find{ |r| r[FRIENDLY_NAME] == friendly_name }
-    role[DB_NAME] if role
+    raise "The '#{ friendly_name }' role couldn't be found. Check that it's " +
+          "declared in #{ File.expand_path(__FILE__) }" unless role
+    role[DB_NAME]
   end
 
   def self.friendly_name(db_name)
     raise_if_missing_key(DB_NAME)
     role = roles.find{ |r| r[DB_NAME] == db_name }
-    role[FRIENDLY_NAME] if role
-  end
-
-  def self.roles
-    [
-      { DB_NAME => 'admin',          FRIENDLY_NAME => 'Project Manager' },
-      { DB_NAME => 'admin',          FRIENDLY_NAME => 'System Admin'    },
-      { DB_NAME => 'Member',         FRIENDLY_NAME => 'Member'          }
-    ]
+    raise "The '#{ db_name }' role couldn't be found. Check that it's " +
+          "declared in #{ File.expand_path(__FILE__) }" unless role
+    role[FRIENDLY_NAME]
   end
 end
