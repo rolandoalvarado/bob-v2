5c9f01180cfddd727c64b1c0401d84e944783e54
Mark Maglana <mmaglana@gmail.com>
2012-05-28 09:34:53 +0800

    Write Create a User test scripts

--- features/2_step_definitions/users_steps.rb
+++ features/2_step_definitions/users_steps.rb
@@ -108,47 +108,142 @@ Then /^s?he will not be able to log in$/ do
   }
 end
 
-Then /^I Can Create a user$/ do
-  user = CloudObjectBuilder.attributes_for(:user, :name => Unique.username('davos'))
+Then /^I can create a user$/i do
+  new_user = CloudObjectBuilder.attributes_for(:user, :username => Unique.username('new'))
+  me       = @me
 
   steps %{
-    * Ensure that a user with username #{ user.name } does not exist
-    * Click the Logout button if currently logged in
+    * Delete the user named #{ new_user.name } at exit
 
+    * Ensure that a user with username #{ new_user.name } does not exist
+    * Ensure that a test project is available for use
+    * Ensure that I have a role of Project Manager in the project
+
+    * Click the Logout button if currently logged in
     * Visit the Login page
-    * Fill in the Username field with #{ @current_user.name }
-    * Fill in the Password field with #{ @current_user.password }
+    * Fill in the Username field with #{ me.name }
+    * Fill in the Password field with #{ me.password }
     * Click the Login button
 
     * Click the Users link
     * Current page should be the Users page
-
     * Click the New User button
-    * Fill in the Username field with #{ user.name }
-    * Fill in the Email field with #{ user.email }
-    * Fill in the Password field with #{ user.password }
+    * Fill in the Username field with #{ new_user.name }
+    * Fill in the Email field with #{ new_user.email }
+    * Fill in the Password field with #{ new_user.password }
     * Choose the 2nd item in the Primary Project dropdown
     * Check the Project Manager checkbox
     * Click the Create User button
     * The New User form should not be visible
-    * The #{ user.name } user row should be visible
-
-    * Register user #{ user.name } for deletion on exit
+    * The #{ new_user.name } user row should be visible
   }
 end
 
-Then /^I Cannot Create a user$/ do
+Then /^I cannot create a user$/i do
+  me = @me
+
   steps %{
     * Click the Logout button if currently logged in
 
     * Visit the Login page
-    * Fill in the Username field with #{ @current_user.name }
-    * Fill in the Password field with #{ @current_user.password }
+    * Fill in the Username field with #{ me.name }
+    * Fill in the Password field with #{ me.password }
     * Click the Login button
 
     * The Users link should not be visible
   }
 end
+
+
+Then /^I can create a user with attributes (.+), (.+), (.+), (.+), and (.+)$/i do |username, email, password, primary_project, is_pm_or_not|
+  new_user = CloudObjectBuilder.attributes_for(
+               :user,
+               :username => Unique.username(username),
+               :email    => Unique.email(email),
+               :password => password
+             )
+  primary_project_choice = case choice
+                           when '(Any)'
+                             '2nd'
+                           when '(None)'
+                             '1st'
+                           end
+
+  check_or_uncheck = (is_pm_or_not == "Yes" ? "Check" : "Uncheck")
+  me       = @me
+
+  steps %{
+    * Delete the user named #{ new_user.name } at exit
+
+    * Ensure that a user with username #{ new_user.name } does not exist
+    * Ensure that a test project is available for use
+    * Ensure that I have a role of Project Manager in the project
+
+    * Click the Logout button if currently logged in
+    * Visit the Login page
+    * Fill in the Username field with #{ me.name }
+    * Fill in the Password field with #{ me.password }
+    * Click the Login button
+
+    * Click the Users link
+    * Current page should be the Users page
+    * Click the New User button
+    * Fill in the Username field with #{ new_user.name }
+    * Fill in the Email field with #{ new_user.email }
+    * Fill in the Password field with #{ new_user.password }
+    * Choose the #{ primary_project_choice } item in the Primary Project dropdown
+    * #{ check_or_uncheck } the Project Manager checkbox
+    * Click the Create User button
+    * The New User form should not be visible
+    * The #{ new_user.name } user row should be visible
+  }
+end
+
+
+Then /^I cannot create a user with attributes (.+), (.+), (.+), (.+), and (.+)$/i do |username, email, password, primary_project, is_pm_or_not|
+  new_user = CloudObjectBuilder.attributes_for(
+               :user,
+               :username => Unique.username(username),
+               :email    => Unique.email(email),
+               :password => password
+             )
+  primary_project_choice = case choice
+                           when '(Any)'
+                             '2nd'
+                           when '(None)'
+                             '1st'
+                           end
+
+  check_or_uncheck = (is_pm_or_not == "Yes" ? "Check" : "Uncheck")
+  me       = @me
+
+  steps %{
+    * Delete the user named #{ new_user.name } at exit
+
+    * Ensure that a user with username #{ new_user.name } does not exist
+    * Ensure that a test project is available for use
+    * Ensure that I have a role of Project Manager in the project
+
+    * Click the Logout button if currently logged in
+    * Visit the Login page
+    * Fill in the Username field with #{ me.name }
+    * Fill in the Password field with #{ me.password }
+    * Click the Login button
+
+    * Click the Users link
+    * Current page should be the Users page
+    * Click the New User button
+    * Fill in the Username field with #{ new_user.name }
+    * Fill in the Email field with #{ new_user.email }
+    * Fill in the Password field with #{ new_user.password }
+    * Choose the #{ primary_project_choice } item in the Primary Project dropdown
+    * #{ check_or_uncheck } the Project Manager checkbox
+    * Click the Create User button
+    * The New User form should be visible
+    * A New User Form Error Message element should be visible
+  }
+end
+
 
 Then /^I [Cc]an [Dd]elete (?:that|the) user (.+)$/ do |username|
   steps %{
