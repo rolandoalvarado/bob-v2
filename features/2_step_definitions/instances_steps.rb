abf50239d615575137aaed411a3445734b1a1bde
Rolando Alvarado <RoRroland@gmail.com>
2012-07-23 18:26:09 +0800

    Fixes for MCF-19

--- features/2_step_definitions/instances_steps.rb
+++ features/2_step_definitions/instances_steps.rb
@@ -511,6 +511,7 @@ Then /^I [Cc]an [Rr]esume the instance$/ do
     * Click the instance menu button for instance #{ instance.id }
     * Click the resume instance button for instance #{ instance.id }
 
+    * Wait 60 seconds
     * The instance #{ instance.id } should be of active status
   }
 end
