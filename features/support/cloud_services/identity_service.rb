1f5f412aef46faca636ad53df30ee0f246ac649a
Rolando Alvarado <RoRroland@gmail.com>
2012-09-06 17:32:58 +0800

    Removed comment-out script.

--- features/support/cloud_services/identity_service.rb
+++ features/support/cloud_services/identity_service.rb
@@ -154,20 +154,6 @@ class IdentityService < BaseCloudService
     end
   end
 
-#  def ensure_user_exists(attributes)
-#    user = find_user_by_name(attributes[:name])
-#    unless user
-#      user = create_user(attributes) 
-#    else
-#      if attributes[:password] != nil && user.password != attributes[:password]
-#        user.password = attributes[:password]
-#        user.update_password(attributes[:password])
-#      end
-#    end
-#   
-#    user
-#  end
-
   def ensure_user_exists(attributes)
     user = find_user_by_name(attributes[:name])
     user = create_user(attributes) unless user
