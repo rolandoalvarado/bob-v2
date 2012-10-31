dfc55d5fd2513964ca8be31ba46ada16ae731d63
Rolando Alvarado <RoRroland@gmail.com>
2012-10-31 19:04:10 +0800

    MCF-189 Failed 'Create a project'

--- features/3_lib/helpers/projects_helpers.rb
+++ features/3_lib/helpers/projects_helpers.rb
@@ -2,6 +2,10 @@ def test_project_name
   Unique.project_name('project')
 end
 
+def test_project_desc
+  'Project Description for Test Project'
+end
+
 def default_project_name
   Unique.project_name('project')
 end
